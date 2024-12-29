b2b478f58fd2d38156124ee94d0973e360286903
邓滨杰 <1264440273@qq.com>
2024-12-29 13:38:17 +0800

    更改

--- read-admin/src/test/java/com/reader/infrastructure/security/TestJwtTokenProvider.java
+++ read-admin/src/test/java/com/reader/infrastructure/security/TestJwtTokenProvider.java
@@ -3,6 +3,8 @@ package com.reader.infrastructure.security;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+import com.reader.domain.user.User;
+
 @Component
 @Profile("test")
 public class TestJwtTokenProvider extends JwtTokenProvider {
@@ -13,6 +15,9 @@ public class TestJwtTokenProvider extends JwtTokenProvider {
     
     // 可以添加一些测试专用的方法
     public String createTestToken(String userId) {
-        return generateAccessToken(userId);
+        User testUser = User.builder()
+            .id(Long.parseLong(userId))
+            .build();
+        return generateAccessToken(testUser);
     }
 } 
